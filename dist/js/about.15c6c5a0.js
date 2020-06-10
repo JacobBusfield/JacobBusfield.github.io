5dbe1d58b69a780755f15f87ce40c0691a3680da
Jacob.Busfield <Jacob.Busfield@simomics.com>
2020-06-10 12:59:46 +0100

    Add dist folder

---
+++ dist/js/about.15c6c5a0.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["about"],{f820:function(t,n,e){"use strict";e.r(n);var a=function(){var t=this,n=t.$createElement,e=t._self._c||n;return e("v-container",[e("v-row",{staticClass:"text-center"},[t._v(" About Page ")])],1)},o=[],r={name:"About"},u=r,c=e("2877"),s=e("6544"),l=e.n(s),i=e("a523"),w=e("0fd9"),b=Object(c["a"])(u,a,o,!1,null,null,null);n["default"]=b.exports;l()(b,{VContainer:i["a"],VRow:w["a"]})}}]);
+//# sourceMappingURL=about.15c6c5a0.js.map
