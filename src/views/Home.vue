6a9d04a822bd6ee19b14ee2b47866198aaa30d27
Jacob.Busfield <Jacob.Busfield@simomics.com>
2021-01-22 15:50:26 +0000

    updating some descriptions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,21 +7,18 @@
       Welcome to my blog:
     </div>
 
-    <Entry title="Battle Cards" :img="require('@/assets/cards.svg')"
-      link="https://jacobbusfield.github.io/BattleCards/">
-      This is a description. With
-      <span style="font-weight:bold">peerjs</span> and
-      <span style="font-weight:bold">dynamoDB</span>. This is a work in
-      progress!
-    </Entry>
-
     <Entry title="Guitar" :img="require('@/assets/guitar.svg')" link="https://jacobbusfield.github.io/Guitar/">
-      This is also a description. This is a work in progress!
+      A thrown together Karplus-Strong guitar synthesiser based upon the original ActionScipt from André Michelle and
+      blog post by Matthew Rahtz (<a href="http://amid.fish/" target="_blank">amid.fish</a>). Full credit for this
+      project must go to them. The code is among of the worst I've written, but hopefully the app itself is interesting
+      (Jun 2020).
     </Entry>
 
     <Entry title="The Island" :img="require('@/assets/castle.svg')" link="https://jacobbusfield.github.io/TheIsland/">
-      A game I made solo for a 24 hour Game Jam event. 2-player local game. Made using <span
-        style="font-weight:bold">Phaser</span> in a time before ES6.
+      A game I made alone for a 24 hour Game Jam event. It is a 2-player local game made using <span
+        style="font-weight:bold">Phaser</span> in a time before ES6. As it was made for a game jam, don't expect great
+      code
+      here! (Dec 2016).
     </Entry>
 
     <Entry title="Under Construction" :img="require('@/assets/logo.svg')">
